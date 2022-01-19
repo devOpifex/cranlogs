5b3db0013965292895a9c63aa8369f8a9ecf5ad5
Devin Pastoor <devin.pastoor@gmail.com>
2022-01-19 15:20:24 -0500

    refactor: simplify daily to not need a specific flag packages
    instead, let the packages be the arguments passed.
    I took the liberty of also not including a default,
    however could make a "default" by just checking
    if no args and then setting that value.
    
    This also removes a global variable and makes
    daily() easier to test as can pass it custom args

--- cmd/daily.go
+++ cmd/daily.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/devOpifex/cranlogs/internal/color"
@@ -10,29 +11,42 @@ import (
 )
 
 var dailyPeriod string
-var dailyPkg string
 
 func daily(_ *cobra.Command, args []string) {
-	daily, err := data.GetDaily(dailyPeriod, dailyPkg)
-
-	if err != nil {
-		color.PrintError(err.Error())
-		os.Exit(0)
+	if len(args) == 0 {
+		log.Fatal("no package specified")
 	}
+	// TODO: this still has some issues in how its handled
+	// as if a specific package fails, it will cause the entire process to
+	// exit then. A different pattern could be to aggregate errors for
+	// failed packages, then print out successful ones, then at the end
+	// the error(s) for the failed ones
+	for _, pkg := range args {
+		daily, err := data.GetDaily(dailyPeriod, pkg)
+		if err != nil {
+			color.PrintError(err.Error())
+			// given an error should not exit with a 0 exit code
+			os.Exit(-1)
+		}
 
-	fmt.Printf("Package: %v %v %v\n", color.Yellow, daily.Package, color.Reset)
-	for _, v := range daily.Downloads {
-		fmt.Printf("%v%v%v: %v\n", color.Cyan, v.Day, color.Reset, v.Downloads)
+		fmt.Printf("Package: %v %v %v\n", color.Yellow, daily.Package, color.Reset)
+		for _, v := range daily.Downloads {
+			fmt.Printf("%v%v%v: %v\n", color.Cyan, v.Day, color.Reset, v.Downloads)
+		}
 	}
 }
 
 func newDailyCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "daily",
-		Short: "Get daily downloads of a package",
-		Run:   daily,
+		Short: "Get daily downloads packages",
+		Long: `
+			cranlogs daily dplyr rlang
+			// can also specify the custom period
+			cranlogs daily --period=last-month dplyr rlang 
+		`,
+		Run: daily,
 	}
 	cmd.Flags().StringVarP(&dailyPeriod, "period", "p", "last-week", "time period")
-	cmd.Flags().StringVarP(&dailyPkg, "package", "a", "dplyr", "package name")
 	return cmd
 }
