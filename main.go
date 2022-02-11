896f8b22a81e3abfa5da398d2db6ed1c4eb6d025
Devin Pastoor <devin.pastoor@gmail.com>
2022-02-11 09:12:28 -0500

    feat: add version information

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "github.com/devOpifex/cranlogs/cmd"
 
+var (
+	version string = "dev"
+	// goreleaser will also inject these if desireable to use
+	// commit  string = "none"
+	// date    string = "unknown"
+)
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
